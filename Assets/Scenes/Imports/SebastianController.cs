f4e99f9de68d1ce0eea41f618859483c58e1c91e
James <james.michael.westbrook@gmail.com>
2020-05-17 12:54:34 -0500

    Basic detection.
    Many thanks to Sebastian Lague for his useful tutorial on math and editor usage for this.

---
+++ Assets/Scenes/Imports/SebastianController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SebastianController : MonoBehaviour
+{
+    public float movespeed = 6;
+    Rigidbody rb;
+    Camera viewCam;
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        viewCam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = viewCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCam.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * movespeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+}
