d2cc3fe1d23ba9b21c1649791a2c21a148a7ecae
James <james.michael.westbrook@gmail.com>
2020-06-05 20:47:43 -0500

    Basic leentween implementation

---
+++ Assets/Scripts/UI/ScaleTween.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        OnClose();
+    }
+    public void OnClose()
+    {
+        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.5f);
+    }
+
+    // Update is called once per frame
+    void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
