4d51868ad8513bd2398c3711c8ac649150e3c2cf
decayingdev <38790193+JamesMWestbrook@users.noreply.github.com>
2020-06-13 12:50:35 -0500

    Fixing up excess files that don't need to be public on github to save space

---
+++ Assets/ListTesting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTesting : MonoBehaviour
+{
+    public List<float> OriginalList;
+
+    public void Start()
+    {
+        List<float> TempList = OriginalList;
+        TempList.Add(3.65f);
+        TempList.Insert(0,72.9f);
+    }
+}
