98dda819306c00aa992a6a8bf4006ba044921dee
decayingdev <38790193+JamesMWestbrook@users.noreply.github.com>
2020-07-17 22:38:26 -0500

    Custom speed for Actor Rotating

--- Assets/Scripts/AI/SOs/TurnTowardAction.cs
+++ Assets/Scripts/AI/SOs/TurnTowardAction.cs
@@ -1,26 +1,36 @@
-﻿using System;
+﻿using InControl.UnityDeviceProfiles;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 [CreateAssetMenu(fileName = "TurnToward", menuName = "DD/Actions/TurnToward")]
 public class TurnTowardAction : DecayingDev.Action
 {
-    public override int ArgumentCount => 1;
-
+    public override int ArgumentCount => 2;
+    float speed = 1f;
     public override void Execute(string[] args, GameObject user)
     {
-        int arg = Int32.Parse(args[0]);
+        int index = Int32.Parse(args[0]);
+        Debug.Log("arg length " + args.Length);
+        if (args.Length == 2)
+        {
+            speed = float.Parse(args[1]);
+
+        }
+        else { speed = 1f; }
         Transform target;
-        if(arg == -1)
+        if(index == -1)
         {
             target = GameManager.Instance.playerController.transform;
         }
         else
         {
-            target = GameManager.Instance.Actors[arg].transform;
+            target = GameManager.Instance.Actors[index].transform;
         }
-
-        user.GetComponent<RotateActor>().target = target;
+        RotateActor rotateActor = user.GetComponent<RotateActor>();
+         rotateActor.target= target;
+        rotateActor.speed = speed;
         Actor actor = user.GetComponent<Actor>();
         actor.ExecuteAction(actor.ActionIndex + 1);
     }
