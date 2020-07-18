8367967dc9c5a20fe469677aeb0298df2b387b30
decayingdev <38790193+JamesMWestbrook@users.noreply.github.com>
2020-07-17 22:24:58 -0500

    Turning Action Implemented

---
+++ Assets/Scripts/AI/SOs/TurnTowardAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "TurnToward", menuName = "DD/Actions/TurnToward")]
+public class TurnTowardAction : DecayingDev.Action
+{
+    public override int ArgumentCount => 1;
+
+    public override void Execute(string[] args, GameObject user)
+    {
+        int arg = Int32.Parse(args[0]);
+        Transform target;
+        if(arg == -1)
+        {
+            target = GameManager.Instance.playerController.transform;
+        }
+        else
+        {
+            target = GameManager.Instance.Actors[arg].transform;
+        }
+
+        user.GetComponent<RotateActor>().target = target;
+        Actor actor = user.GetComponent<Actor>();
+        actor.ExecuteAction(actor.ActionIndex + 1);
+    }
+
+}
